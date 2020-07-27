5ca4493c806f8be6bd621022b35a0839ce692e22
YudaiTanaka <null01242020@gmail.com>
2020-07-27 19:50:16 +0900

    plot demo

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 10, 100)
+y = x + np.random.randn(100) 
+
+plt.plot(x, y, label="test")
+
+plt.legend()
+
+plt.show()
